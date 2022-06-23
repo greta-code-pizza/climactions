a44b67c34f719a411749f2154e983f3d544e43d6
Nala77 <alan.adam0197@gmail.com>
2022-06-23 12:00:11 +0200

    valider avec un r

--- app/Views/frontend/layouts/searchbar.php
+++ app/Views/frontend/layouts/searchbar.php
@@ -1,6 +1,6 @@
 <form method="POST" action="pageArticle&search=true">
     <div class="bar-search">
         <input type="search" class="input-search" name="query" placeholder="Rechercher un article">
-        <input type="submit" class="btn-search" value="Validez">
+        <input type="submit" class="btn-search" value="Valider">
     </div>
 </form>
